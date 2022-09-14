81932698cf69cf20a345e8eebec4d06456d9d9fd
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-09-14 18:14:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ let D = "65%"
 let F = "30%"
 var grade = A //set grade
 console.log("Setting Grade: "+ grade)
+for(var i=0;i < texts.length; i++){
+document.getElementsByClassName('percent')[i].innerHTML = grade;//Client Side Change
+}
