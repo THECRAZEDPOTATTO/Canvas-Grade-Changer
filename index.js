8c4001fe6a33e25c2060ddb9bb6054b59cf101de
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-09-14 18:09:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+var texts = document.getElementsByClassName('percent');
+for(var i=0;i < texts.length; i++){
+   console.log(texts[i].textContent);
+}
 let A = "100%"
 let B = "80%"
 let C = "70%"
 let D = "65%"
 let F = "30%"
 var grade = A //set grade
-console.log("Setting Grade: "+grade)
-var texts = document.getElementsByClassName('precent');
-
-for(var i=0;i < texts.length; i++){
-   console.log(texts[i].textContent);
-}
+console.log("Setting Grade: "+ grade)
