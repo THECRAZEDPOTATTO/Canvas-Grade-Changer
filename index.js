e413197fc2535acc981003676d6078c6640ff86b
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-11-23 23:04:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var texts = document.getElementsByClassName('percent');
-let A = "100%"
-let B = "80%"
-let C = "70%"
-let D = "65%"
-let F = "30%"
-var grade = A //set grade
-console.log("Setting Grade: "+ grade)
-for(var i=0;i < texts.length; i++){
-document.getElementsByClassName('percent')[i].innerHTML = grade;//Client Side Change
+const rA = [90,100];
+const rB = [80,89];
+const rC = [70,79];
+const rD = [60,69];
+const rF = [30,59];
+var grade = rA //set grade
+const realistic_Grades = true;
+if (realistic_Grades) {
+  const highest = Math.max(...grade);
+  const lowest = Math.min(...grade);
+  const random = Math.floor(Math.random() * (highest - lowest + 1)) + lowest;
+  for(var i=0;i < texts.length; i++){
+    document.getElementsByClassName('percent')[i++].innerHTML = random+"%";
+  }
+} 
+else{
+  console.log("Setting Grade: "+ grade)
+  for(var i=0;i < texts.length; i++){
+  document.getElementsByClassName('percent')[i].innerHTML = grade;}//Client Side Change
 }
+
